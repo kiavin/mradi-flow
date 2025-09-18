7f5addd092f33ab071d4026426b80dc8ef0a6311
Itachi <bscs157j2022@students.tum.ac.ke>
2025-09-18 15:36:22 +0300

    done

--- modules/project/views/Dashboard/index.vue
+++ modules/project/views/Dashboard/index.vue
@@ -541,7 +541,7 @@ onMounted(fetchReport)
                     d="M13,20H11V8L5.5,13.5L4.08,12.08L12,4.16L19.92,12.08L18.5,13.5L13,8V20Z"
                   ></path>
                 </svg>
-                {{ contributionGrowth }}% this month
+                Recent Transactions
               </p>
             </div>
           </b-card-header>
